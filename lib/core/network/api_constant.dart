6fcf7f0a63211cdc87786ce557d7feab9245ec4d
mohamedmagdy2301 <mohamed.magdy@pure-soft.com>
2025-06-07 19:06:39 +0300

    Remove merge conflict markers and clean up ApiConstant class

--- lib/core/network/api_constant.dart
+++ lib/core/network/api_constant.dart
@@ -1,10 +1,6 @@
 class ApiConstant {
   static const String baseUrl = "https://engmohamedshr18.pythonanywhere.com";
   static const String signup = "/accounts/auth/users/";
-<<<<<<< HEAD
   static const String story = "/tweets/stories/";
-}
-=======
   static const String login = "/accounts/auth/jwt/create/";
 }
->>>>>>> 9d23f33c995e1729aa8ade1a15f399b785aa7a6f
